3c0a2989cd11b6897a1174cf31f7ec7e6dcfaa5b
kiu_maghlakelidze.saba <maghlakelidze.saba@kiu.edu.ge>
2024-01-30 19:33:07 +0400

    added custom not-found page and instant loading states

---
+++ app/tickets/[id]/not-found.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function notFound() {
+  return (
+    <main className="text-center">
+        <h2 className="text-3xl">Ticket couldn't be found</h2>
+        <p>We could not find the ticket you were looking for.</p>
+        <p>Go back to the <Link href="/">Tickets</Link></p>
+    </main>
+  )
+}
