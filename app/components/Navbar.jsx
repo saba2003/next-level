43e69f6c1d80c49c7397186a962824d3fc255911
kiu_maghlakelidze.saba <maghlakelidze.saba@kiu.edu.ge>
2024-02-06 19:58:23 +0400

    added active on nav and fun facts at loads

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -5,6 +8,8 @@ import React from 'react'
 import Logo from './Tailwind_CSS_Logo.svg.png'
 
 export default function Navbar() {
+  const pathname = usePathname()
+
   return (
     <nav>
         <Image 
@@ -13,9 +18,8 @@ export default function Navbar() {
           width={70}
           quality={100}
         />
-        <h1>navbar</h1>
-        <Link href="/">Dashboard</Link>
-        <Link href="/tickets">tickets</Link>
+        <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">Dashboard</Link>
+        <Link className={`link ${pathname === '/tickets' ? 'active' : ''}`} href="/tickets">tickets</Link>
     </nav>
   )
 }
--- app/loading.jsx
+++ app/loading.jsx
@@ -1,8 +1,25 @@
-export default function Loading() {
+async function getFacts() {
+  const res = await fetch('https://api.api-ninjas.com/v1/facts?limit=1', {
+      headers: {
+        'X-Api-Key': 'Y4dtSPaGHzleoh69H9Pihg==nWxGT5Xt0jkQRZxm'
+      },
+      next: {
+        revalidate: 0 //revalidate waits a certain time before re-renders updated data
+      }
+  })
+
+  return res.json()
+}
+
+export default async function Loading() {
+  const facts = await getFacts()
+
   return (
     <main className="text-center">
         <h2 className="text-primary">Loading...</h2>
-        <p>fun fact: ... </p>
+        {facts.length === 1 && facts.map(fact => (
+          <p>fun fact: {fact.fact}</p> 
+        ))}
     </main>
   )
 }
